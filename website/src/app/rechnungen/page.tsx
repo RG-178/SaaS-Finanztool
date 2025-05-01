6d00620e47fd0f844c57c33624e2c9b78ff07cdd
Riccardo Ambühl <riccardo.ambuehl@icloud.com>
2025-05-01 22:30:39 +0200

    update

--- website/src/app/rechnungen/page.tsx
+++ website/src/app/rechnungen/page.tsx
@@ -5,13 +5,10 @@ import { useEffect, useState } from 'react';
 import './style.css';
 
 export default function Dashboard() {
-  const allPages = ['dashboard', 'kunden', 'rechnungen', 'finanzen', 'einstellungen'];
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [page, setPage] = useState('dashboard');
 
   useEffect(() => {
     setIsSidebarOpen(sessionStorage.getItem('sidebar') === 'true' || false);
-    setPage(window.location.href.split('/').filter(Boolean).pop());
   }, []);
 
   useEffect(() => {
@@ -43,8 +40,7 @@ export default function Dashboard() {
                   <div className="h-[1px] w-[calc(100%-1.4rem)] bg-[rgba(255,255,255,0.3)]"></div>
               </div>
             </div>
-            {}
-            <a href="/dashboard" className='flex items-center gap-[12px] pt-[20px] pb-[20px] cursor-pointer text-[#00a2a2] transition-colors duration-300 ease-in-out'>
+            <a href="/dashboard" className='flex items-center gap-[12px] pt-[20px] pb-[20px] cursor-pointer text-[rgba(255,255,255,0.6)] hover:text-[rgba(255,255,255,1)] transition-colors duration-300 ease-in-out'>
               <svg className="ml-[22px] min-w-[28px] min-h-[28px] size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
               <span className='text whitespace-nowrap overflow-hidden text-[14px] font-bold'>DASHBOARD</span>
             </a>
@@ -52,7 +48,7 @@ export default function Dashboard() {
               <svg className="ml-[22px] min-w-[28px] min-h-[28px] size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" /></svg>
               <span className='text whitespace-nowrap overflow-hidden text-[14px] font-bold'>KUNDEN</span>
             </a>
-            <a href="/rechnungen" className='flex items-center gap-[12px] pt-[20px] pb-[20px] cursor-pointer text-[rgba(255,255,255,0.6)] hover:text-[rgba(255,255,255,1)] transition-colors duration-300 ease-in-out'>
+            <a href="/rechnungen" className='flex items-center gap-[12px] pt-[20px] pb-[20px] cursor-pointer text-[#00a2a2] transition-colors duration-300 ease-in-out'>
               <svg className="ml-[22px] min-w-[28px] min-h-[28px] size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" /></svg>
               <span className='text whitespace-nowrap overflow-hidden text-[14px] font-bold'>RECHNUNGEN</span>
             </a>
@@ -74,7 +70,7 @@ export default function Dashboard() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" /></svg>
               )}
             </button>
-            <h1 className='text-[rgba(0,0,0,1)] capitalize'>{page}</h1>
+            <h1 className='text-[rgba(0,0,0,1)]'>Rechnungen</h1>
             <div className='flex ml-auto bg-[rgba(222,222,222,0.3)] mr-[15.2px] transition-colors duration-300 ease-in-out rounded-[4px] searching'>
               <input type="text" placeholder='Search...' className='p-[11px] h-[40px] w-[170px] text-[#66615b] !outline-none' onFocus={() => {document.querySelector('.searching')?.classList.remove('bg-[rgba(222,222,222,0.3)]'); document.querySelector('.searching')?.classList.add('bg-[rgba(222,222,222,0.5)]')}} onBlur={() => {document.querySelector('.searching')?.classList.add('bg-[rgba(222,222,222,0.3)]'); document.querySelector('.searching')?.classList.remove('bg-[rgba(222,222,222,0.5)]')}} />
               <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#66615b" className="mt-auto mb-auto mr-[8px]"><path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>
